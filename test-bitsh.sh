ecb2e62279383fec61ab3919e339abe8e603854e
Brian Oxley <boxley@thoughtworks.com>
2016-05-22 10:13:58 -0500

    Ignores, cleanups

--- test-bitsh.sh
+++ test-bitsh.sh
@@ -2,8 +2,9 @@
 
 {
     pushd $(dirname $0)
-    root_dir=$(pwd -P)
+    readonly root_dir=$(pwd -P)
     popd
 } >/dev/null
+readonly dot_script=$root_dir/.$(basename $0)
 
-$root_dir/run-from-url https://raw.githubusercontent.com/binkley/shell/master/plain-bash-testing/run-tests $root_dir/.$(basename $0) "$@"
+bash $root_dir/run-from-url https://raw.githubusercontent.com/binkley/shell/master/plain-bash-testing/run-tests $dot_script "$@"
