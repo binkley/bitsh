09949b61cec1241e5f4ec6c09332b91df8a50ae4
Brian Oxley <boxley@thoughtworks.com>
2016-05-22 10:29:21 -0500

    Understand -o and -u flags

--- test-bitsh.sh
+++ test-bitsh.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+offline=
+update=
+args=("$@")
+for i in $(seq 0 $(($# - 1)))
+do
+    arg="${args[i]}"
+    case $arg in
+    -o ) offline=-o ; unset args[i] ;;
+    -u ) update=-u ; unset args[i] ;;
+    -* ) ;;
+    * ) break ;;
+    esac
+done
+set -- "${args[@]}"
+
 {
     pushd $(dirname $0)
     readonly root_dir=$(pwd -P)
@@ -7,4 +22,4 @@
 } >/dev/null
 readonly dot_script=$root_dir/.$(basename $0)
 
-bash $root_dir/run-from-url https://raw.githubusercontent.com/binkley/shell/master/plain-bash-testing/run-tests $dot_script "$@"
+$root_dir/run-from-url $offline $update https://raw.githubusercontent.com/binkley/shell/master/plain-bash-testing/run-tests $dot_script "$@"
